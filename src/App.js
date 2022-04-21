d37851748e397e1d9c34389a4fb03d2721d115b3
Seung-wan <tmddhks0104@gmail.com>
2022-04-21 11:57:18 +0900

    Project Setup by CRA

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Toggle from './components/Toggle';
+import Slider from './components/Slider';
+import Input from './components/Input';
+function App() {
+  return (
+    <div className="App">
+      {/* <Toggle />
+      <br />
+      <Slider />
+      <br /> */}
+      <Input />
+      <br />
+    </div>
+  );
+}
+
+export default App;
