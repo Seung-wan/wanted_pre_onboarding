1213dd4f98632e9b162631ebce4e3d2978a36a79
Seung-wan <tmddhks0104@gmail.com>
2022-04-21 23:22:51 +0900

    feat. Slider

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,16 +1,36 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 
 const Slider = () => {
   const [percent, setPercent] = useState(0);
+  const [colorCircle, setColorCircle] = useState(0);
+
+  const clickPercent = (per, colorCircle) => {
+    setPercent(per);
+    setColorCircle(colorCircle);
+  };
 
   const changeSlider = (evt) => {
+    console.log(Math.floor(Number(evt.target.value) / 25));
+    setColorCircle(Math.floor(Number(evt.target.value) / 25));
     setPercent(evt.target.value);
   };
 
+  const renderCircle = (colorCircle) => {
+    const arr = Array.from({ length: 5 }, (_, i) => {
+      if (i === 4) return <div key={i} className="circle color"></div>;
+
+      return i < colorCircle ? (
+        <div key={i} className="circle color"></div>
+      ) : (
+        <div key={i} className="circle"></div>
+      );
+    });
+    return arr;
+  };
   return (
     <div>
       <div className="percent">
-        <span>{percent}</span>
+        <span className="black">{percent}</span>
         <span>%</span>
       </div>
       <div className="range-circle">
@@ -22,23 +42,19 @@ const Slider = () => {
           max="100"
           value={percent}
           onChange={changeSlider}
+          style={{ backgroundSize: `${percent}% 100%` }}
         />
-        <div className="circle"></div>
-        <div className="circle"></div>
-        <div className="circle"></div>
-        <div className="circle"></div>
-        <div className="circle"></div>
+        {renderCircle(colorCircle)}
       </div>
 
       <div className="percent-button">
-        <div onClick={() => setPercent(1)}>1%</div>
-        <div onClick={() => setPercent(25)}>25%</div>
-        <div onClick={() => setPercent(50)}>50%</div>
-        <div onClick={() => setPercent(75)}>75%</div>
-        <div onClick={() => setPercent(100)}>100%</div>
+        <div onClick={() => clickPercent(1, 0)}>1%</div>
+        <div onClick={() => clickPercent(25, 1)}>25%</div>
+        <div onClick={() => clickPercent(50, 2)}>50%</div>
+        <div onClick={() => clickPercent(75, 3)}>75%</div>
+        <div onClick={() => clickPercent(100, 4)}>100%</div>
       </div>
     </div>
   );
 };
-
 export default Slider;
