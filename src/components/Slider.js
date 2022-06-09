3bb774a38d388be70b8673ae18b879f6de2f8bac
유승완 <51105841+Seung-wan@users.noreply.github.com>
2022-06-09 23:12:16 +0900

    refactor react import

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 
 const Slider = () => {
   const [percent, setPercent] = useState(0);
--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,5 +1,3 @@
-import React from 'react';
-
 const Toggle = () => {
   return (
     <div style={{ marginBottom: '50px' }}>
